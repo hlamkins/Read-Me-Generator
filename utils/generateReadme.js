8961438e7cc432533fc701eb1217346faf487b25
Heather Lamkins <lamkinsheather@gmail.com>
2020-12-03 21:33:10 -0600

    Update formatting

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -1,42 +1,40 @@
 function generateReadme(answers) {
     return `
-    # ${answers.title}\n
+# ${answers.title}
 
-    ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)\n
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
 
-    ## Description
-        ${answers.description}\n
+## Description
+${answers.description}
 
-    ## Table of Contents
-    -[Description](#description)
-    -[Installation](#installation)  
-    -[Usage](#usage)
-    -[License](#license)  
-    -[Contribution](#contribution)
-    -[Questions](#questions)
+## Table of Contents
+-[Description](#description)
+-[Installation](#installation)  
+-[Usage](#usage)
+-[License](#license)  
+-[Contribution](#contribution)
+-[Questions](#questions)
 
-    ## Installation
-    ${answers.installation}
+## Installation
+${answers.installation}
 
-    ## Usage
-    ${answers.usage}
+## Usage
+${answers.usage}
 
-    ## License
-    ![badge](https://img.shields.io/badge/license-${answers.license}-lightblue)\n
-    This application is covered by tthe ${answers.license} license.
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-lightblue)
+This application is covered by tthe ${answers.license} license.
 
-    ## Contributing
-    ${answers.contribution}
+## Contributing
+${answers.contribution}
 
-    ## Questions
-    Find me on GitHub: [${answers.github}](https://github.com/${answers.github})\n
+## Questions
+Find me on GitHub: [${answers.github}](https://github.com/${answers.github})
     
-    Email me with any questions at ${answers.email}\n
+Email me with any questions at ${answers.email}
 
-    #### This README was created using [Read-Me-Generator](https://github.com/hlamkins/Read-Me-Generator)
-
-
-    `;
+#### This README was created using [Read-Me-Generator](https://github.com/hlamkins/Read-Me-Generator)
+`;
 
 }
 
