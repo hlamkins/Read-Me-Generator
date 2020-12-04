24e6f0a0fe2efb7337886ccebf1c3b85f5b87a21
Heather Lamkins <lamkinsheather@gmail.com>
2020-12-03 18:43:38 -0600

    Finished adding all variables to readme

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -15,7 +15,25 @@ function generateReadme(answers) {
     -[Contribution](#contribution)
     -[Questions](#questions)
 
+    ## Installation
+    ${answers.installation}
 
+    ## Usage
+    ${answers.usage}
+
+    ## License
+    ![badge](https://img.shields.io/badge/license-${answers.license}-lightblue)\n
+    This application is covered by tthe ${answers.license} license.
+
+    ## Contributing
+    ${answers.contribution}
+
+    ## Questions
+    Find me on GitHub: [${answers.github}](https://github.com/${answers.github})\n
+    
+    Email me with any questions at ${answers.email}\n
+
+    #### This README was created using [Read-Me-Generator](https://github.com/hlamkins/Read-Me-Generator)
 
 
     `;
